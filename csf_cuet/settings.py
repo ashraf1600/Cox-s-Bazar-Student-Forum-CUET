d13ebdde5936534994e9cb81a0bf018c6c457d52
Ashraful Islam <ashrafdcc1502@gmail.com>
2026-06-21 13:02:50 +0600

    Fix Django storage backend

--- csf_cuet/settings.py
+++ csf_cuet/settings.py
@@ -80,7 +80,7 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS = [BASE_DIR / 'static']
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
-MEDIA_URL = 'media/'
+MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
 LOGIN_URL = 'core:login'
@@ -182,8 +182,10 @@ def unread_count(request):
 
 
 STORAGES = {
+    "default": {
+        "BACKEND": "django.core.files.storage.FileSystemStorage",
+    },
     "staticfiles": {
-        "BACKEND":
-        "whitenoise.storage.CompressedManifestStaticFilesStorage",
+        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
     },
 }
